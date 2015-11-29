9cc3f6fd88bbfdf93b038d5a0a9929125bf306ca
Massimo Santini <santini@di.unimi.it>
2015-11-29 18:05:22 +0100

    Adede GA

--- app.js
+++ app.js
@@ -238,7 +238,7 @@ function start() {
 	else
 		pausa_risposta = 500;
 
-	if ( voices ) {
+	if ( voices && voices.length > 0 ) {
 		voce = parseInt( form.voce.value );
 		if ( voce == - 1 ) voce = Math.floor( Math.random() * voices.length );
 	} else
@@ -258,5 +258,15 @@ function start() {
 
 /* Twitter */
 !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
+
+/* Google Analytics */
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga( 'create', 'UA-377250-22', 'auto', { 'storage': 'none' } );
+ga( 'send', 'pageview' );
 
 }
