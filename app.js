eab894ac79c41dc5ae28df052183a59e589056c2
Massimo Santini <santini@di.unimi.it>
2015-11-30 11:05:51 +0100

    Removed cookie generating code – to abide the EU cookie law

--- app.js
+++ app.js
@@ -257,10 +257,13 @@ function start() {
 	step();
 }
 
-/* Twitter */
+/*
+
+// Twitter
+
 !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
 
-/* Google Analytics */
+// Google Analytics
 
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -269,5 +272,7 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 
 ga( 'create', 'UA-377250-22', 'auto', { 'storage': 'none' } );
 ga( 'send', 'pageview' );
+
+*/
 
 }
